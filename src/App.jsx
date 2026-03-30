518aeb057864bac03ad0ad023cf156a58bc11abc
PeyaraFul <arakash022@gmail.com>
2026-03-30 15:49:03 +0600

    tool section and tolls card created successfully with json data

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,24 @@ import Navbar from "./assets/hero/Navbar";
 import PremiumTools from "./assets/tools/PremiumTools";
 
 
+
+
+
+
+ const loadProducts = async () => {
+    const response = await fetch('/product.json');
+    const data = await response.json();
+    return data ;
+    // console.log(data);
+    // data.map(products => {
+    //     console.log(products.name);
+    // });
+};
+
+const productPromise = loadProducts();
+
+
+
 function App() {
   return (
     <>
@@ -12,7 +30,7 @@ function App() {
       <Navbar></Navbar>
       <Banner></Banner>
       <Dashboard></Dashboard>
-      <PremiumTools></PremiumTools>
+      <PremiumTools productPromise={productPromise}></PremiumTools>
 
 
     </>
